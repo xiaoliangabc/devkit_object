c2bc8ce6c299999be685cac1ed448cdf98adfd64
xiaoliangabc <995117727@qq.com>
2020-10-04 16:23:08 +0800

    temp

--- mapping/prase.py
+++ mapping/prase.py
@@ -4,6 +4,13 @@
 import shutil
 import os
 
+'''
+KITTI Object 数据集 mapping 逻辑
+图像索引：000000-007480
+train_rand：对0-7480先加1，然后将其打乱，每一个数字在整个序列中的位置（打乱后的顺序）就是其在train_mapping中的行数
+train_mapping：每一行代表了所对应原始数据的采集时间和帧数
+'''
+
 # 读取打乱数据
 rand_path = '/home/wxl/self_driving/detection_tracking/kitti/detection/devkit_object/mapping/train_rand.txt'
 with open(rand_path, "r") as f:
